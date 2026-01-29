99abfd738027c3b3eaf9dd835825f35956102272
habibursoftvence <habibur@softvencedelta.com>
2026-01-29 17:28:11 +0600

    update

--- database/migrations/2026_01_28_090638_add_image_and_role_to_users_table.php
+++ database/migrations/2026_01_28_090638_add_image_and_role_to_users_table.php
@@ -13,10 +13,21 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('image')->nullable()->after('password');
-            $table->tinyInteger('type')->default(0)->comment('0 = User, 1 = Admin')->after('image');
-             $table->string('phone')->nullable()->after('type');
-             $table->string('role')->nullable()->after('phone');
-             $table->string('status')->nullable()->comment('0 = Inactive, 1 = Active')->after('role');
+            $table->tinyInteger('type')->default(0)->comment('0 = User, 1 = Admin, 2 = Merchant')->after('image');
+            $table->string('phone')->nullable()->after('type');
+            $table->string('role')->nullable()->after('phone');
+            $table->string('status')->nullable()->comment('0 = Inactive, 1 = Active')->after('role');
+            $table->string('website_domain')->nullable()->after('status');
+            $table->string('address')->nullable()->after('website_domain');
+            $table->string('platform_access')->nullable()->comment('0 = Disabled, 1 = Enabled')->after('address');
+            $table->string('current_package')->nullable()->after('platform_access');
+            $table->string('package_duration')->nullable()->after('current_package');
+            $table->string('package_start_date')->nullable()->after('package_duration');
+            $table->string('package_end_date')->nullable()->after('package_start_date');
+            $table->string('package_expire_date')->nullable()->after('package_end_date');
+            $table->string('remaining_day')->nullable()->after('package_expire_date');
+            $table->string('package_status')->nullable()->after('remaining_day');
+
         });
     }
 
