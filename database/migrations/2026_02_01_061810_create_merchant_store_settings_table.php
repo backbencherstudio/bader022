a2cb86ad6493026accf6430dcd547f0587a324ed
habibursoftvence <habibur@softvencedelta.com>
2026-02-01 15:40:58 +0600

    update

---
+++ database/migrations/2026_02_01_061810_create_merchant_store_settings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('merchant_store_settings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onDelete('cascade');
+            $table->string('store_name');
+            $table->string('business_name')->nullable();
+            $table->unsignedBigInteger('business_id')->nullable();
+            $table->enum('business_category', ['salon_beauty','home_services','health','fitness_pro_gym','others',])->nullable();
+            $table->text('business_address')->nullable();
+            $table->string('country', 100)->nullable();
+            $table->string('city', 100)->nullable();
+            $table->string('time_zone', 100)->default('UTC');
+            $table->string('currency', 10)->default('USD');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('merchant_store_settings');
+    }
+};
