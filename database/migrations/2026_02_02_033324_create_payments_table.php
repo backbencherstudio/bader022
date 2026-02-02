c684c0d3ba79eed51e15e8e48825fa005c97d594
habibursoftvence <habibur@softvencedelta.com>
2026-02-02 09:54:31 +0600

    update

---
+++ database/migrations/2026_02_02_033324_create_payments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('subscription_id')->constrained('subscriptions')->onDelete('cascade');
+            $table->decimal('amount', 10, 2);
+            $table->string('currency', 5)->default('SAR');
+            $table->enum('payment_method', ['Stripe', 'Paypal']);
+            $table->string('transaction_id')->unique();
+            $table->enum('status', ['pending', 'successfull', 'failed'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
