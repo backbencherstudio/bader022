2c243c065ec3651fb4437abb9a1f59ac9268a241
habibursoftvence <habibur@softvencedelta>
2026-02-26 14:04:06 +0600

    update

--- app/Http/Controllers/Merchant/BookingController.php
+++ app/Http/Controllers/Merchant/BookingController.php
@@ -390,7 +390,7 @@ class BookingController extends Controller
                 $booking = Booking::find($bookingId);
 
             if ($booking) {
-              
+
                 $booking->update(['status' => 'confirmed']);
 
                     MerchantPayment::updateOrCreate(
