c06c2e68825aec03c41a3dcc9e0a0e05ba21435d
KamruzzamanSVD <kamruzzaman@softvencedelta.com>
2026-04-07 11:20:10 +0600

    merchant setting business hour update

--- database/migrations/2026_02_01_061810_create_merchant_store_settings_table.php
+++ database/migrations/2026_02_01_061810_create_merchant_store_settings_table.php
@@ -14,14 +14,14 @@ return new class extends Migration
         Schema::create('merchant_store_settings', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('store_name');
+            $table->string('store_name')->nullable();
             $table->string('business_logo')->nullable();
             $table->enum('business_category', ['salon_beauty','home_services','health','fitness_pro_gym','others',])->nullable();
             $table->text('business_address')->nullable();
             $table->string('country', 100)->nullable();
             $table->string('city', 100)->nullable();
-            $table->string('time_zone', 100)->default('UTC');
-            $table->string('currency', 10)->default('USD');
+            $table->string('time_zone', 100)->default('Asia/Riyadh');
+            $table->string('currency', 10)->default('SAR');
             $table->timestamps();
         });
     }
